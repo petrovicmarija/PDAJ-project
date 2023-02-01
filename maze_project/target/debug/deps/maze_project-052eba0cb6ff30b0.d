165c7c066ccd394a93792a97232cebbe203c8d72
Marija Petrovic <marijap@protonmail.com>
2023-02-01 11:24:20 +0100

    add initial project structure and maze.txt file

---
+++ maze_project/target/debug/deps/maze_project-052eba0cb6ff30b0.d
@@ -0,0 +1,5 @@
+/home/marija/Desktop/rust-maze/PDAJ-project/maze_project/target/debug/deps/maze_project-052eba0cb6ff30b0: src/main.rs
+
+/home/marija/Desktop/rust-maze/PDAJ-project/maze_project/target/debug/deps/maze_project-052eba0cb6ff30b0.d: src/main.rs
+
+src/main.rs:
---
+++ maze_project/target/debug/maze_project.d
@@ -0,0 +1,1 @@
+/home/marija/Desktop/rust-maze/PDAJ-project/maze_project/target/debug/maze_project: /home/marija/Desktop/rust-maze/PDAJ-project/maze_project/src/main.rs
